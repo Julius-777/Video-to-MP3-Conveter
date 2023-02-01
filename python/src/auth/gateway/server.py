c02550f7cd10025f1a90b7081377b74cb55f9ae2
Julius Miyumo <jjmiyumo@hotmail>
2023-02-02 07:52:42 +1100

    sqs queue added and upload, download endpoints

--- python/src/auth/gateway/server.py
+++ python/src/auth/gateway/server.py
@@ -4,9 +4,44 @@ from flask_pymongo import PyMongo
 from auth import validate
 from auth_svc import access
 from storage import util
+import channel
 
 server = Flask(__name__)
 server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
 
 mongo = PyMongo(server) #abstracting handling of MongoDB connection
- 
+fs = gridfs.GridFS(mongo.db) #enable handling of files > 16MB
+
+# connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+# channel = connection.channel()
+queue = channel.get_queue("VideoMP3Queue")
+
+@server.route("/login", methods=["POST"])
+def login():  
+  token, err = access.login(request)
+  return token if not err else err
+
+@server.route("/upload", methods=["POST"])
+def upload():
+	access, err = validate.token(request)
+	access = json.loads(access)
+
+	if access["admin"]:
+		if len(request.files) != 1:
+			return "Exactly 1 file required", 400 
+
+		for _, f in request.files.items():
+			err = util.upload(f, fs, queue, access)
+			if err: 
+				return err
+		
+		return "success", 200
+	else:
+		return "not authorized", 401
+
+@server.route("/download", methods=["GET"])
+def download():
+	pass
+
+if __name__ == "__main__":
+	server.run(host="0.0.0.0", port=8080)
